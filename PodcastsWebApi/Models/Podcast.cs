5946d025ab7d6acf3d97c801f3321143dcfe54ac
Oana <oan.max@gmail.com>
2020-06-29 19:25:03 +0300

    User and Podcast entities added.

---
+++ PodcastsWebApi/Models/Podcast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PodcastsWebApi.Models
+{
+    public class Podcast
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Author { get; set; }
+        public int NumberOfItems { get; set; }
+        public string Picture { get; set; } 
+    }
+}
---
+++ PodcastsWebApi/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PodcastsWebApi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
